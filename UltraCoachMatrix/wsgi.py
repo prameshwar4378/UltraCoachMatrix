1d9aec82ccb5c70176d80923501f7e19510459ec
Rameshwar Pawar <prameshwar4378@gmail.com>
2026-06-08 18:27:10 +0530

    wsgi issue fixed

--- UltraCoachMatrix/wsgi.py
+++ UltraCoachMatrix/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UltraCoachMatrix.settings')
 
